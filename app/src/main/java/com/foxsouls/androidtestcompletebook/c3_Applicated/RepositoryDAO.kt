371ac23fbb0aba14ab997f34b34ea44410a7e7ea
felix <foxsouls-Android@yahoo.co.jp>
2020-04-23 21:33:38 +0900

    3.1.1 Windows > Mac

---
+++ app/src/main/java/com/foxsouls/androidtestcompletebook/c3_Applicated/RepositoryDAO.kt
@@ -0,0 +1,17 @@
+package com.foxsouls.androidtestcompletebook.c3_Applicated
+
+import androidx.room.*
+
+@Dao
+interface RepositoryDAO {
+    @Insert
+    fun insertAll(vararg repositories: Repository)
+
+    @Query("SELECT * FROM repository WHERE owner = owner")
+    fun findByOwner(owner: String): List<Repository>
+}
+
+@Database(entities = [Repository::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun repositoryDao(): RepositoryDAO
+}
